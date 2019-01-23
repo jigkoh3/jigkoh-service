ecc6bc27d24fec79ba781ddb9db54276f261c92b
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2019-01-23 14:48:36 +0700

    edit #2

--- src/modules/share/test/routes.js
+++ src/modules/share/test/routes.js
@@ -144,7 +144,7 @@ describe('Share CRUD routes tests', function () {
 
     });
 
-    xit('should be share get not use token', (done)=>{
+    xit('should be share get not use token 1', (done)=>{
         request(app)
         .get('/api/shares')
         .expect(403)
